d7a41bf0a0a9483312eaa09b04a170a6bd5755c4
David Nuon <davidnuongm@gmail.com>
2022-10-26 22:09:45 -0700

    WebC install plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+
 module.exports = function (config) {
     config.addPassthroughCopy({ './src/_static/style.css': 'style.css' })
     config.addPassthroughCopy({ './src/_static/bg.png': 'bg.png' })
-  
+    config.addPlugin(pluginWebc)
+
     return {
       dir: {
         input: './src',
